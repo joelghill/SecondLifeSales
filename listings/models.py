a9467175d4e3391b17e78ed543534f115eaa56d7
Joel Hill <joelghill@protonmail.com>
2022-03-22 20:22:53 -0600

    Added listing model with initial migration

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Listing(models.Model):
+
+    # Text Descriptions
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    # Pricing 
+    price = models.FloatField()
+    price_fixed = models.BooleanField(default=False)
+
+    # Publication 
+    published = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
