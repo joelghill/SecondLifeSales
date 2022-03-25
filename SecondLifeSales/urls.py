049364f0950776c61c0ab24a21bf9762a57d891a
Joel Hill <joelghill@protonmail.com>
2022-03-24 19:52:50 -0600

    Fixed URL issue

--- SecondLifeSales/urls.py
+++ SecondLifeSales/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from SecondLifeSales.views import HomeView
+from common.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
