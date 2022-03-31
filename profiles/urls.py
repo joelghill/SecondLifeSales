3a5fdfc24e933161ee7ab9c791657c6896882faf
Joel Hill <joelghill@protonmail.com>
2022-03-31 17:30:33 -0600

    Adding register view with CreateUserForm

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from profiles.views import CreateProfileView
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', CreateProfileView.as_view(), name='register')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+
+class CreateProfileView(FormView):
+
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = "registration/register.html"
