998b9d84cbe1e1693d6dc7e17b4bd880b816fc88
Joel Hill <joelghill@protonmail.com>
2022-04-08 18:51:34 -0600

    Implemented search

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import ListView, DetailView, TemplateView
+from django.db.models import Q
 from listings.models import Listing
 
 
 class ListingsListView(ListView):
     model = Listing
-    
+
     # paginate_by = 100  # if pagination is desired
 
     def get_context_data(self, **kwargs):
@@ -25,10 +26,21 @@ class ListingsHomeView(TemplateView):
     template_name = "listings/listings_home.html"
 
     def get_context_data(self, **kwargs):
+
+        q = self.request.GET.get("q")
+
         context = super().get_context_data(**kwargs)
-        context["listings"] = self.get_listings()
+        context["listings"] = self.get_listings(search_query=q)
 
         return context
 
-    def get_listings(self):
-        return Listing.objects.filter(published=True)
+    def get_listings(self, search_query=None):
+        search = Listing.objects.filter(published=True)
+
+        if search_query:
+            search = search.filter(
+                Q(description__icontains=search_query) | 
+                Q(title__icontains=search_query)
+            )
+
+        return search
